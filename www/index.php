dec1afc9f439f6645143417d711a33bd73d485cd
Daniel Costa <costa@vixinet.ch>
2015-02-05 14:34:31 +0200

    init

---
+++ www/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="vendors/css/bootstrap.min.css" rel="stylesheet">
+		<link href="vendors/css/main.css" rel="stylesheet">
+		<title>Togetherly Ltd</title>
+	</head>
+	<body class="home">
+		<div class="section corporate">
+			<div class="container">
+				<h1>togetherly <sub>Ltd</sub></h1>
+			</div>
+		</div>
+
+		<div class="section intro">
+			<div class="container">
+				<div class="col-sm-24">
+					<div class="visible-xs visible-sm">
+						<p class="quote">
+							<span><span class="quote">&#8220;</span> Logic</span> will get you from A to B. Imagination will take you <span>everywhere. <span class="quote">&#8221;</span></span>
+						</p>
+					</div>
+
+					<div class="hidden-xs hidden-sm">
+						<p class="quote line-1">
+							<span class="quote">&#8220;</span>
+							Logic will get you from A to B.
+						</p>
+						<p class="quote line-2">
+							Imagination will take you everywhere.
+							<span class="quote">&#8221;</span>
+						</p>
+					</div>
+					<p class="author">- Albert Einstein</p>
+				</Div>
+			</div>
+		</div>
+
+			
+		<div class="section product">
+			<div class="container-fluid level-11">
+				<div class="col-sm-12 picture-cell">
+					<img class="product" src="images/product-crop.png" />
+				</div>
+				<div class="col-sm-12 desc-cell">
+					<h2>BLURRY</h2>
+					<p>
+						Blurry makes app dating personal again. It is our conviction that the success of dating is all about connecting through the subtle details, such as the way people react to things or the way of behaving in conversations. Blurry allows people to primarily evaluate their counterparts based on these details, which makes our users focus on what really matters: <strong>real human connection</strong>.
+					</p>
+					<div class="website">
+						<a href="http://www.goblurry.com">Blurry's website</a>
+					</div>
+					<!--
+					<div class="social">
+						<a href="https://www.facebook.com/blurryapp">
+							<img src="images/social-facebook.png" />
+						</a>
+						<a href="https://twitter.com/GoBlurry">
+							<img src="images/social-twitter.png" />
+						</a>
+						<a href="https://www.youtube.com/channel/UCBaj1uR4G52oMSeDjFbtsSA">
+							<img src="images/social-youtube.png" />
+						</a>
+					</div>
+				-->
+				</div>
+			</div>
+		</div>
+
+		<div class="section founders">
+			<div class="header">
+				<h2>founders</h2>
+				<div class="container profile hidden-xs">
+					<div class="row">
+						<div class="cell col-sm-8 col-md-8 col-lg-6 col-lg-offset-3">
+							<img class="profile" src="images/team-dc-thumb.png"/>
+							<h3>Daniel Costa</h3>
+							<h4>Chief Executive Officer</h4>
+						</div>
+						<div class="cell col-sm-8 col-md-8 col-lg-6 ">
+							<img class="profile" src="images/team-jk-thumb.png"/>
+							<h3>Julian Klinger</h3>
+							<h4>Chief Operations Officer</h4>
+						</div>
+						<div class="cell col-sm-8 col-md-8  col-lg-6 ">
+							<img class="profile" src="images/team-vv-thumb.png"/>
+							<h3>Vanessa Vandy</h3>
+							<h4>Advisor</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container body-part">
+				<div class="row">
+					<div class="cell col-sm-8 col-md-8 col-lg-6 col-lg-offset-3">
+						<div class="visible-xs">
+							<img class="profile" src="images/team-dc-thumb.png"/>
+							<h3>Daniel Costa</h3>
+							<h4>Chief Executive Officer</h4>
+						</div>
+						<div class="social">
+							<a href="mailto:d.costa@togetherly.nz"><img src="images/social-email.png"/></a>
+							<a href="https://www.linkedin.com/pub/daniel-costa/70/105/b43"><img src="images/social-linkedin.png"/></a>
+							<a href="skype:dc.daniel.costa?call"><img src="images/social-skype.png"/></a>
+						</div>
+						<p>
+							Daniel's decision-making and team-leading skills allow the company to constantly move forward without delay. His habit of aiming high and following up on progress will make any team push further than they first thought was possible. Daniel has an extensive background in managing time-sensitive projects and developing software for crucial applications. His eye for details and aim for perfection will allow Blurry to be well prepared for future challenges.
+						</p>
+					</div>
+					<div class="cell col-sm-8 col-md-8 col-lg-6 ">
+						<div class="visible-xs">
+							<img class="profile" src="images/team-jk-thumb.png"/>
+							<h3>Julian Klinger</h3>
+							<h4>Chief Operations Officer</h4>
+						</div>
+						<div class="social">
+							<a href="mailto:j.klinger@togetherly.nz"><img src="images/social-email.png"/></a>
+							<a href="https://www.linkedin.com/pub/julian-klinger/81/aa1/4b7"><img src="images/social-linkedin.png"/></a>
+							<a href="skype:notquiteeasytofindanick"><img src="images/social-skype.png"/></a>
+						</div>
+						<p>
+							Julian's natural ability to act as a mediator makes it easy for him to bring people to the same wavelength. This will help Blurry move forward as a balanced and strong team. His skill of distinguishing what's important from what's not allows him to define a clear direction for the product development. His patience to work long and hard inspires the whole team to do the same.
+						</p>
+					</div>
+					<div class="cell col-sm-8 col-md-8 col-lg-6 ">
+						<div class="visible-xs">
+							<img class="profile" src="images/team-vv-thumb.png"/>
+							<h3>Vanessa Vandy</h3>
+							<h4>Advisor</h4>
+						</div>
+						<div class="social">
+							<a href="mailto:v.vandy@togetherly.nz"><img src="images/social-email.png"/></a>
+							<a href="https://www.linkedin.com/pub/vanessa-vandy/37/89a/134"><img src="images/social-linkedin.png"/></a>
+							<a href="skype:vanessavandy"><img src="images/social-skype.png"/></a>
+						</div>
+						<p>
+							Vanessa's curiosity of getting to know new people helps Blurry create business contacts from a variety of different areas. With a background in filmmaking, she has gained marketing skills and knowledge to help elevate Blurry's marketing plan. Her will to figure out the most optimal definitions of how reality really plays out, will help Blurry to define and communicate their business more efficiently.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-58433289-2', 'auto');
+			ga('send', 'pageview');
+		</script>
+	</body>
+</html>
